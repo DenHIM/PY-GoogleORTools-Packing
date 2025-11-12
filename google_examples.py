818eb5eec3628bcbe82c432be9e254e0e8d19027
DenHIM <denis.guryev@hotmail.com>
2025-11-12 10:50:03 -0500

    Added cost minimization

--- google_examples.py
+++ google_examples.py
@@ -150,3 +150,85 @@ class BinPackingMultiBin:
             print("Time = ", solver.WallTime(), " milliseconds")
         else:
             print("The problem does not have an optimal solution.")
+
+
+class BinPackingCost:
+    data = {}
+
+    def __init__(self, weights, bin_capacities=[100], bin_costs=[1]):
+        if len(bin_capacities) != len(bin_costs):
+            raise ValueError("bin_capacities and bin_costs must have the same length.")
+        
+        self.init_data_model(weights, bin_capacities, bin_costs)
+
+    def init_data_model(self, weights, bin_capacities, bin_costs):
+        """Create the data for the example."""
+        self.data["weights"] = weights
+        self.data["items"] = list(range(len(weights)))
+        self.data["bins"] = []
+        for j, bin_cap in enumerate(bin_capacities):
+            self.data["bins"] += [(bin_cap, bin_costs[j]) for _ in self.data["items"]]
+
+    def run_solve(self, show_IDs=True):
+        solver = pywraplp.Solver.CreateSolver('SCIP')
+        if not solver:
+            return
+
+        # Variables
+        # x[i, j] = 1 if item i is packed in bin j.
+        x = {}
+        for i in self.data["items"]:
+            for j, _ in enumerate(self.data["bins"]):
+                x[(i, j)] = solver.IntVar(0, 1, "x_%i_%i" % (i, j))
+
+        # y[j] = 1 if bin j is used.
+        y = {}
+        for j, _ in enumerate(self.data["bins"]):
+            y[j] = solver.IntVar(0, 1, "y[%i]" % j)
+            
+        # Constraints
+        # Each item must be in exactly one bin.
+        for i in self.data["items"]:
+            solver.Add(sum(x[i, j] for j, _ in enumerate(self.data["bins"])) == 1)
+
+        # The amount packed in each bin cannot exceed its capacity.
+        for j, bin_cap in enumerate(self.data["bins"]):
+            solver.Add(
+                sum(x[(i, j)] * self.data["weights"][i] for i in self.data["items"])
+                <= y[j] * bin_cap[0]
+            )
+
+        # Objective: minimize the number of bins used.
+        solver.Minimize(solver.Sum([y[j] * bin_cap[1] for j, bin_cap in enumerate(self.data["bins"])]))
+
+        # Solving the problem.
+        print(f"Solving with {solver.SolverVersion()}")
+        status = solver.Solve()
+        if status == pywraplp.Solver.OPTIMAL:
+            num_bins = 0
+            total_cost = 0
+            for j, _ in enumerate(self.data["bins"]):
+                if y[j].solution_value() == 1:
+                    bin_items = []
+                    bin_items_weight = []
+                    bin_weight = 0
+                    for i in self.data["items"]:
+                        if x[i, j].solution_value() > 0:
+                            bin_items.append(i)
+                            bin_items_weight.append(self.data["weights"][i])
+                            bin_weight += self.data["weights"][i]
+                    if bin_items:
+                        num_bins += 1
+                        total_cost += self.data["bins"][j][1]
+                        print("Bin number", j)
+                        if show_IDs:
+                            print("  Items packed (IDs):", bin_items)
+                        print("  Items packed (Weight):", bin_items_weight)
+                        print("  Total weight:", bin_weight)
+                        print()
+            print()
+            print("Number of bins used:", num_bins)
+            print("Total cost:", total_cost)
+            print("Time = ", solver.WallTime(), " milliseconds")
+        else:
+            print("The problem does not have an optimal solution.")
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from google_examples import BinPackingSimple, BinPackingMultiBin
+from google_examples import BinPackingSimple, BinPackingMultiBin, BinPackingCost
 
-problem = BinPackingSimple([4, 7, 1, 4, 2, 1, 3, 5, 7, 3], bin_capacity=10)
-problem.run_solve(show_IDs=False)
+data = [4, 7, 1, 4, 2, 1, 3, 5, 7, 3]
+
+#problem = BinPackingSimple([4, 7, 1, 4, 2, 1, 3, 5, 7, 3], bin_capacity=10)
+#problem.run_solve(show_IDs=False)
 #problem = BinPackingSimple([48, 30, 19, 36, 36, 27, 42, 42, 36, 24, 30], bin_capacity=100)
 #problem.run_solve(show_IDs=False)
 #problem = BinPackingMultiBin([7, 3, 3, 3, 10], bin_capacities=[7, 10])
 #problem.run_solve(show_IDs=False)
-problem = BinPackingMultiBin([4, 7, 1, 4, 2, 1, 3, 5, 7, 3], bin_capacities=[7,10])
+#problem = BinPackingMultiBin([4, 7, 1, 4, 2, 1, 3, 5, 7, 3], bin_capacities=[7,10])
+#problem.run_solve(show_IDs=False)
+problem = BinPackingSimple(data, bin_capacity=10)
+problem.run_solve(show_IDs=False)
+problem = BinPackingMultiBin(data, bin_capacities=[7, 10])
+problem.run_solve(show_IDs=False)
+problem = BinPackingCost(data, bin_capacities=[7,10], bin_costs=[2,4])
 problem.run_solve(show_IDs=False)
